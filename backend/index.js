130bab3b26020b4c0ce61ef0442e284fa7bb65d0
abhishek9640 <abhishekgupta9640@gmail.com>
2023-09-21 21:49:06 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,62 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
 
 const app = express();
+
+//Middleware for parsing request body
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request)
     return response.status(234).send('Welcome to MERN Stack')
+});
+
+// Route for Save a new Book
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+
+        const createdBook = await Book.create(newBook);
+
+        return response.status(201).send(createdBook);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 })
 
-mongoose 
+//Route for Get All Books from database
+app.get('/books', async (request, response) =>{
+    try {
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books
+
+        });
+    }catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+mongoose
     .connect(mongoDBURL)
     .then(() => {
         console.log('App i connected to the database');
@@ -17,6 +64,6 @@ mongoose
             console.log(`App is listening to port: ${PORT}`);
         })
     })
-    .catch((error) =>{
+    .catch((error) => {
         console.log(error)
-    });
+    }); 
